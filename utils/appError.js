21529b2acde8537c3a2b26bd2d53f2efbe33c600
Elmamis69 <60937298+Elmamis69@users.noreply.github.com>
2024-03-19 03:05:54 -0700

    Segundo punto y septimo listo
    • El ruteo del servicio no debe estar en el Index de la aplicación
    • Se debe implementar autenticación por JWT.

---
+++ utils/appError.js
@@ -0,0 +1,41 @@
+const e = require("express");
+const winston= require('winston')
+
+//almacenar errores en archivo
+const logger = winston.createLogger({
+    level:'error',
+    format: winston.format.json(),
+    transports:[
+        new winston.transports.File({filename:'error.log'})
+    ]
+})
+
+class AppError extends Error{
+
+    constructor(message,statusCode) {
+        super(message)
+        this.statusCode = statusCode
+        this.status = `${statusCode}`.startsWith('4')|| `${statusCode}`.startsWith('5') ? 'fail':'error';
+        this.isOperational = true
+
+        Error.captureStackTrace(this,this.constructor)
+    }
+}
+
+const globalErrorHandler=(err,req,res,next) =>{
+    err.statusCode = err.statusCode||500;
+    err.status=err.status  || 'error'
+
+    logger.error(err.message)
+    res.status(err.statusCode).json({
+        statur:err.status,
+        message:err.message,
+        error:err
+    })
+
+};
+
+module.exports ={
+    AppError,
+    globalErrorHandler
+}
